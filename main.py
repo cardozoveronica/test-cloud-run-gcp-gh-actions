fa70d80fecdb5d2dc03cf4e8166de08cf3dcd5f2
veroC <ceibo@macbookpro.lan>
2025-12-08 16:39:37 -0300

    testing

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_customers():
 
 def get_customers_data():
     customers = [
-    {'customer_id': 1, 'name': 'Sara White', 'address': '5866 Fuller Road', 'city': 'Henryport', 'state': 'Maryland', 'zipcode': '28764', 'country': 'United States'},
+    {'customer_id': 1, 'name': 'Shara White', 'address': '5866 Fuller Road', 'city': 'Henryport', 'state': 'Maryland', 'zipcode': '28764', 'country': 'United States'},
     {'customer_id': 2, 'name': 'Brian Davenport', 'address': '2139 Mcguire Estates Apt. 391', 'city': 'North Peter', 'state': 'Alaska', 'zipcode': '85682', 'country': 'United States'},
     {'customer_id': 3, 'name': 'Donald Poole', 'address': '123 Karen Lights', 'city': 'Teresahaven', 'state': 'West Virginia', 'zipcode': '17084', 'country': 'United States'},
     {'customer_id': 4, 'name': 'Casey Conner', 'address': '18792 Gibson Throughway Apt. 646', 'city': 'West Michaelfort', 'state': 'Florida', 'zipcode': '37673', 'country': 'United States'},
